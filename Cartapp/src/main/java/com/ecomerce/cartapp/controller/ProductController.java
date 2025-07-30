177cb94c2c450aed9a44cf558221796e06ac8571
Balamurugan S <132822490+BALAMURUGAN5363@users.noreply.github.com>
2025-07-30 21:41:10 +0530

    Add files via upload

---
+++ Cartapp/src/main/java/com/ecomerce/cartapp/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.ecomerce.cartapp.controller;
+
+import com.ecomerce.cartapp.model.Product;
+import com.ecomerce.cartapp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}

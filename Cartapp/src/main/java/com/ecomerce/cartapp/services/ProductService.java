d35a6486f0eb5940cc642149d3a3af022cb20dfb
Balamurugan S <132822490+BALAMURUGAN5363@users.noreply.github.com>
2025-07-30 21:43:10 +0530

    Add files via upload

---
+++ Cartapp/src/main/java/com/ecomerce/cartapp/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.ecomerce.cartapp.service;
+
+import com.ecomerce.cartapp.model.Product;
+import com.ecomerce.cartapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
